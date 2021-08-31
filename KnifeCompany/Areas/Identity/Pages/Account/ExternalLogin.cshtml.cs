0f1be9cf7434e368d3d62f6992df0be74ef7bf72
A-Bayusuto <bayusuto@gmail.com>
2021-08-31 16:33:30 +0700

    external account can add password

--- KnifeCompany/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
+++ KnifeCompany/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
@@ -61,6 +61,17 @@ namespace KnifeCompany.Areas.Identity.Pages.Account
             public string State { get; set; }
             public string PostalCode { get; set; }
             public string PhoneNumber { get; set; }
+
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            [Display(Name = "Password")]
+            public string Password { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Confirm password")]
+            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            public string ConfirmPassword { get; set; }
         }
 
         public IActionResult OnGetAsync()
@@ -147,7 +158,7 @@ namespace KnifeCompany.Areas.Identity.Pages.Account
 
                 };
 
-                var result = await _userManager.CreateAsync(user);
+                var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
                     await _userManager.AddToRoleAsync(user, SD.Role_User_Indi);
